85fc5e33ed37d06aaa47afd208ffb3c40515d0da
aliabji <aliabji@gmail.com>
2018-08-08 10:55:45 -0400

    updated readme, added separate file for endpoints

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-Endpoints:
-/api/570  <== AMD RX570
-/api/1060 <== Nvidia 1060
+## Setup
 
-Install dependencies with npm i
-Start API with pm2 start app.js -i max
+Install dependencies with `npm i`
+Ensure PM2 is installed on your machine with `npm install pm2 -g`
+Ensure Redis is installed with `npm install redis`
+Launch Redis with `redis-server`
+Start API with `pm2 start app.js -i max`
+
+## Endpoints:
+/api/570   <== AMD RX570
+/api/1060  <== Nvidia 1060
+
+## Notes
+Both endpoints will serve the best coin to mine at the current time against Ethereum. The API will also return supplementary information like the exchange rate (usually with BTC as a trading partner), algorithm used in the calculation and other useful information. Power and equipment costs are not factored into the analysis.
+
+If any other numbers are entered following the '/api/' endpoint, the API will return an error with a message instructing the user to search for either the RX570 or Nvidia 1060.
--- app.js
+++ app.js
@@ -1,41 +1,55 @@
 // require the dependencies we installed
-var app = require('express')();
-var responseTime = require('response-time')
-var axios = require('axios');
-var redis = require('redis');
-var cluster = require('cluster');
+const app = require('express')();
+const responseTime = require('response-time')
+const axios = require('axios');
+const redis = require('redis');
+const compression = require('compression');
+const endpoints = require('./endpoints.js');
 
 // create a new redis client and connect to our local redis instance
-var client = redis.createClient();
+const client = redis.createClient();
 
 // if an error occurs, print it to the console
 client.on('error', function (err) {
     console.log("Error " + err);
 });
 
+// listen on port 4000
 app.set('port', (process.env.PORT || 4000));
+
 // set up the response-time middleware
 app.use(responseTime());
 
+// Compress all routes
+app.use(compression());
+
+// centralized error handler
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send(someErrorMessage)
+});
+
 // call the WhatToMine API to fetch information about the gpu's most profitable coin
-function getMostProfitableCoin(gpu) {
-  var wtmEndpoint = ''
+getMostProfitableCoin = (gpu) => {
+  let wtmEndpoint = ''
 
   if (gpu === "570") {
-    wtmEndpoint = 'https://whattomine.com/coins.json?utf8=%E2%9C%93&adapt_q_280x=0&adapt_q_380=0&adapt_q_fury=0&adapt_q_470=0&adapt_q_480=0&adapt_q_570=1&adapt_570=true&adapt_q_580=0&adapt_q_vega56=0&adapt_q_vega64=0&adapt_q_750Ti=0&adapt_q_1050Ti=0&adapt_q_10606=0&adapt_q_1070=0&adapt_q_1070Ti=0&adapt_q_1080=0&adapt_q_1080Ti=0&eth=true&factor%5Beth_hr%5D=27.90&factor%5Beth_p%5D=120.00&zh=true&factor%5Bzh_hr%5D=0.00&factor%5Bzh_p%5D=0.00&phi=true&factor%5Bphi_hr%5D=13.00&factor%5Bphi_p%5D=120.00&cnh=true&factor%5Bcnh_hr%5D=630.00&factor%5Bcnh_p%5D=110.00&cn7=true&factor%5Bcn7_hr%5D=830.00&factor%5Bcn7_p%5D=110.00&eq=true&factor%5Beq_hr%5D=260.00&factor%5Beq_p%5D=110.00&lre=true&factor%5Blrev2_hr%5D=29500.00&factor%5Blrev2_p%5D=120.00&ns=true&factor%5Bns_hr%5D=700.00&factor%5Bns_p%5D=140.00&tt10=true&factor%5Btt10_hr%5D=8.00&factor%5Btt10_p%5D=115.00&x16r=true&factor%5Bx16r_hr%5D=5.00&factor%5Bx16r_p%5D=100.00&l2z=true&factor%5Bl2z_hr%5D=0.42&factor%5Bl2z_p%5D=110.00&phi2=true&factor%5Bphi2_hr%5D=0.00&factor%5Bphi2_p%5D=0.00&xn=true&factor%5Bxn_hr%5D=0.00&factor%5Bxn_p%5D=0.00&factor%5Bcost%5D=0.0&sort=Profitability24&volume=0&revenue=24h&factor%5Bexchanges%5D%5B%5D=&factor%5Bexchanges%5D%5B%5D=binance&factor%5Bexchanges%5D%5B%5D=bitfinex&factor%5Bexchanges%5D%5B%5D=bittrex&factor%5Bexchanges%5D%5B%5D=cryptobridge&factor%5Bexchanges%5D%5B%5D=cryptopia&factor%5Bexchanges%5D%5B%5D=hitbtc&factor%5Bexchanges%5D%5B%5D=poloniex&factor%5Bexchanges%5D%5B%5D=yobit&dataset=&commit=Calculate';
+    wtmEndpoint = endpoints.amd570;
+  } else if (gpu === "1060") {
+    wtmEndpoint = endpoints.nvidia1060;
   } else {
-    wtmEndpoint = 'https://whattomine.com/coins.json?utf8=%E2%9C%93&adapt_q_280x=0&adapt_q_380=0&adapt_q_fury=0&adapt_q_470=0&adapt_q_480=0&adapt_q_570=0&adapt_q_580=0&adapt_q_vega56=0&adapt_q_vega64=0&adapt_q_750Ti=0&adapt_q_1050Ti=0&adapt_q_10606=1&adapt_10606=true&adapt_q_1070=0&adapt_q_1070Ti=0&adapt_q_1080=0&adapt_q_1080Ti=0&eth=true&factor%5Beth_hr%5D=22.50&factor%5Beth_p%5D=90.00&zh=true&factor%5Bzh_hr%5D=23.00&factor%5Bzh_p%5D=90.00&phi=true&factor%5Bphi_hr%5D=12.00&factor%5Bphi_p%5D=90.00&cnh=true&factor%5Bcnh_hr%5D=450.00&factor%5Bcnh_p%5D=70.00&cn7=true&factor%5Bcn7_hr%5D=430.00&factor%5Bcn7_p%5D=70.00&eq=true&factor%5Beq_hr%5D=270.00&factor%5Beq_p%5D=90.00&lre=true&factor%5Blrev2_hr%5D=20300.00&factor%5Blrev2_p%5D=90.00&ns=true&factor%5Bns_hr%5D=620.00&factor%5Bns_p%5D=90.00&tt10=true&factor%5Btt10_hr%5D=11.00&factor%5Btt10_p%5D=90.00&x16r=true&factor%5Bx16r_hr%5D=5.00&factor%5Bx16r_p%5D=80.00&l2z=true&factor%5Bl2z_hr%5D=1.10&factor%5Bl2z_p%5D=80.00&phi2=true&factor%5Bphi2_hr%5D=2.30&factor%5Bphi2_p%5D=90.00&xn=true&factor%5Bxn_hr%5D=2.10&factor%5Bxn_p%5D=90.00&factor%5Bcost%5D=0.0&sort=Profitability24&volume=0&revenue=24h&factor%5Bexchanges%5D%5B%5D=&factor%5Bexchanges%5D%5B%5D=binance&factor%5Bexchanges%5D%5B%5D=bitfinex&factor%5Bexchanges%5D%5B%5D=bittrex&factor%5Bexchanges%5D%5B%5D=cryptobridge&factor%5Bexchanges%5D%5B%5D=cryptopia&factor%5Bexchanges%5D%5B%5D=hitbtc&factor%5Bexchanges%5D%5B%5D=poloniex&factor%5Bexchanges%5D%5B%5D=yobit&dataset=&commit=Calculate'
+    return Promise.reject('invalid search');
   }
   return axios.get(wtmEndpoint);
-}
+};
 
 // when a user queries the api with a gpu, return most profitable coins
-app.get('/api/:gpu', function(req, res) {
+app.get('/api/:gpu', (req, res) => {
   // get the gpu parameter in the URL
-  var gpu = req.params.gpu;
+  let gpu = req.params.gpu;
 
   // use the redis client to get the cached coins
-  client.hget(gpu, "best", (error, result) => {
+  client.hget(gpu, "best", (error, result, next) => {
       if (result) {
         // the result exists in our cache - return it to our user immediately
         res.send({ "most_profitable_coin": JSON.parse(result) });
@@ -44,26 +58,23 @@ app.get('/api/:gpu', function(req, res) {
         // from the WhatToMine API
         getMostProfitableCoin(gpu)
           .then((data) => {
-            console.log(data)
             return data.data.coins
           })
           .then((coins) => {
             let biggest = new Object;
-            for (var i in coins) {
-              if (coins[i].profitability > biggest.profitability_1hr || biggest.profitability_1hr === undefined) {
-                console.log(coins[i], "here")
-                biggest = {
-                  name: i,
-                  ticker: coins[i].tag,
-                  algorithm: coins[i].algorithm,
-                  block_reward: coins[i].block_reward,
-                  block_time: coins[i].block_time,
-                  difficulty: coins[i].difficulty,
-                  exchange_rate: coins[i].exchange_rate,
-                  estimated_rewards: coins[i].estimated_rewards,
-                  profitability_1hr: `${coins[i].profitability}% compared to Ethereum`
-                }
-              };
+            for (let i in coins) {
+              biggest = {
+                name: i,
+                ticker: coins[i].tag,
+                algorithm: coins[i].algorithm,
+                block_reward: coins[i].block_reward,
+                block_time: coins[i].block_time,
+                difficulty: coins[i].difficulty,
+                exchange_rate: `${coins[i].exchange_rate} against ${coins[i].exchange_rate_curr}`,
+                estimated_rewards: coins[i].estimated_rewards,
+                current_profit: `${coins[i].profitability}% compared to Ethereum`
+              }
+              break
             };
             // store most profitable coin in redis cache
             client.hset(gpu, "best", JSON.stringify(biggest), (err, val) => {
@@ -77,12 +88,12 @@ app.get('/api/:gpu', function(req, res) {
                 res.send({ "most_profitable_coin": biggest});
               };
             });
-          }).catch((response) => {
-            if (response.status === 404){
-              res.send('An error occurred!');
+          }).catch((err) => {
+            if (err === 'invalid search') {
+              res.status(404).send('Please search for either an AMD RX570 or Nvidia 1060')
             } else {
-              res.send(response);
-            };
+              res.status(500).send('An error occurred. Please try again.')
+            }
           });
       };
   });
